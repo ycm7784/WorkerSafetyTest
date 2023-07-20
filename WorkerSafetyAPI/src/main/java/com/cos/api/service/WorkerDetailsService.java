90cd366f862ce2455ee026b0dc3dbd28d180cdbb
ycm7784 <ycm7784@naver.com>
2023-07-20 14:56:36 +0900

    수정

--- WorkerSafetyAPI/src/main/java/com/cos/api/service/WorkerDetailsService.java
+++ WorkerSafetyAPI/src/main/java/com/cos/api/service/WorkerDetailsService.java
@@ -3,7 +3,6 @@ package com.cos.api.service;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.cos.api.domain.WorkerDetails;
